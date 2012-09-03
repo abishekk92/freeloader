234a0bc9280f1f5bcdb6cbb639e084b0ca8aba94
Muralidhar <smuralireddy@gmail.com>
2012-09-03 21:23:05 +0530

    Made it clear

--- windows.bat
+++ windows.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-SET /P fname=Please enter the film/game/song/etc name:
-SET /P format=Please enter the format you want
+SET /P fname=Please enter the film/game/song/etc(one word) name:
+SET /P format=Please enter the format you want(mpeg or mp4 or flc etc)
 start /max http://www.google.com/search?q="+%fname%+"%%20-inurl:(htm%%7Chtml%%7Cphp%%7Cpls%%7Ctxt)%%20intitle:index.of+"last%%20modified"+("+%format%+")%%20
