7a4c032b5d9dda95f97ea19cd5147073b0f70e09
Abishek Bhat <bhat.abishek@gmail.com>
2012-09-22 17:37:55 +0530

    Revert "Support for multi word input string"
    This reverts commit 9005d01fb97e6582741c10e86720d018e38137c4.

--- query.sh
+++ query.sh
@@ -1,4 +1,3 @@
 base_url="http://www.google.com/search?q="
-file_name=$(echo $1|sed s/-/%20/)
-query="$file_name%20-inurl:(htm%7Chtml%7Cphp%7Cpls%7Ctxt)%20intitle:index.of+\"last%20modified\"+($2|%7$3)%20"
+query="$1%20-inurl:(htm%7Chtml%7Cphp%7Cpls%7Ctxt)%20intitle:index.of+\"last%20modified\"+($2|%7$3)%20"
 x-www-browser $base_url$query
