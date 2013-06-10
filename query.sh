6612ff17e503c06d389b52325355d4d973e5aeec
Abishek Bhat <bhat.abishek@gmail.com>
2013-06-11 03:37:08 +0530

    Fixing the issue with escaping the multi word keywords

--- query.sh
+++ query.sh
@@ -1,3 +1,5 @@
 base_url="http://www.google.com/search?q="
-query="$1%20-inurl:(htm%7Chtml%7Cphp%7Cpls%7Ctxt)%20intitle:index.of+\"last%20modified\"+($2|$3)%20"
+escaped=$(echo $1|sed s/" "/%20/g )
+query="$escaped%20-inurl:(htm%7Chtml%7Cphp%7Cpls%7Ctxt)%20intitle:index.of+\"last%20modified\"+($2|$3)%20"
 x-www-browser $base_url$query
+
